87a92a47a449565b0a0a38596b71ce941613777e
Enes Seval <enesseval@outlook.com>
2023-03-19 00:11:41 +0300

    add rel=noreferrer in a tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,13 @@ import { faInstagram, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-
 function Footer() {
 	return (
 		<div className="footer">
-			<a href="https://github.com/enesseval" target="_blank">
+			<a href="https://github.com/enesseval" target="_blank" rel="noopener noreferrer">
 				<FontAwesomeIcon icon={faGithub} size="4x" />
 			</a>
-			<a href="https://www.instagram.com/sofwarediaries/" target="_blank">
+			<a href="https://www.instagram.com/sofwarediaries/" target="_blank" rel="noopener noreferrer">
 				<FontAwesomeIcon icon={faInstagram} size="4x" />
 			</a>
-			<a href="https://www.linkedin.com/in/enes-seval-162679230/" target="_blank">
+			<a href="https://www.linkedin.com/in/enes-seval-162679230/" target="_blank" rel="noopener noreferrer">
 				<FontAwesomeIcon icon={faLinkedin} size="4x" />
 			</a>
 		</div>
